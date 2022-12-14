2404e098e8ef05d302606f8c5f53ec114a699e81
Phol <Pholibertlim@gmail.com>
2022-12-15 04:01:01 +0800

    fixed bug

--- project_4_5/client/vite.config.js
+++ project_4_5/client/vite.config.js
@@ -5,11 +5,6 @@ import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      "/adminUser":"http://localhost:8000/adminUser",
-    },
-  },
   plugins: [react()],
   resolve: {
     alias: {
